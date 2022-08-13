8353f86b10d6400bb89aaf91f82626451d384037
IT Park <IT Park@DESKTOP-MF63OBN>
2022-08-13 17:04:47 +0500

    Add project files.

---
+++ ang_kk/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ang_kk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mOdeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enKkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tema1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kkEnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tema1ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.Red;
+            this.guna2TextBox1.BorderRadius = 10;
+            this.guna2TextBox1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.guna2TextBox1.BorderThickness = 0;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 36F);
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.Red;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(307, 293);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(596, 74);
+            this.guna2TextBox1.TabIndex = 1;
+            this.guna2TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.guna2TextBox1.TextOffset = new System.Drawing.Point(0, -7);
+            this.guna2TextBox1.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 20;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Red;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(416, 421);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(349, 74);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.TabStop = false;
+            this.guna2Button1.Text = "Jardem";
+            this.guna2Button1.TextOffset = new System.Drawing.Point(0, -10);
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 20;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.Red;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(205, 138);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(762, 96);
+            this.guna2Button2.TabIndex = 3;
+            this.guna2Button2.TabStop = false;
+            this.guna2Button2.TextOffset = new System.Drawing.Point(0, -5);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mOdeToolStripMenuItem,
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1160, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mOdeToolStripMenuItem
+            // 
+            this.mOdeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enKkToolStripMenuItem,
+            this.kkEnToolStripMenuItem});
+            this.mOdeToolStripMenuItem.Name = "mOdeToolStripMenuItem";
+            this.mOdeToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.mOdeToolStripMenuItem.Text = "MOde";
+            // 
+            // enKkToolStripMenuItem
+            // 
+            this.enKkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tema1ToolStripMenuItem});
+            this.enKkToolStripMenuItem.Name = "enKkToolStripMenuItem";
+            this.enKkToolStripMenuItem.Size = new System.Drawing.Size(138, 26);
+            this.enKkToolStripMenuItem.Text = "En - Kk";
+            // 
+            // tema1ToolStripMenuItem
+            // 
+            this.tema1ToolStripMenuItem.Name = "tema1ToolStripMenuItem";
+            this.tema1ToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.tema1ToolStripMenuItem.Text = "Tema - 1";
+            this.tema1ToolStripMenuItem.Click += new System.EventHandler(this.tema1ToolStripMenuItem_Click);
+            // 
+            // kkEnToolStripMenuItem
+            // 
+            this.kkEnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tema1ToolStripMenuItem1});
+            this.kkEnToolStripMenuItem.Name = "kkEnToolStripMenuItem";
+            this.kkEnToolStripMenuItem.Size = new System.Drawing.Size(138, 26);
+            this.kkEnToolStripMenuItem.Text = "Kk - En";
+            // 
+            // tema1ToolStripMenuItem1
+            // 
+            this.tema1ToolStripMenuItem1.Name = "tema1ToolStripMenuItem1";
+            this.tema1ToolStripMenuItem1.Size = new System.Drawing.Size(150, 26);
+            this.tema1ToolStripMenuItem1.Text = "Tema - 1";
+            this.tema1ToolStripMenuItem1.Click += new System.EventHandler(this.tema1ToolStripMenuItem1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1160, 675);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "3";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mOdeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enKkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tema1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kkEnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tema1ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ang_kk/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ang_kk
+{
+    public partial class Form1 : Form
+    {
+
+        string[] vs;
+        string[] vj;
+        string filename = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        Excel.Application xlApp = new Excel.Application();
+        int inc = 0, mode = 0;
+        
+        Random random = new Random();
+        int tt = 1;
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (mode == 1)
+            {
+                guna2TextBox1.Text = vj[inc].Substring(0, tt);
+                tt++;
+            }
+            else if (mode == 2)
+            {
+                guna2TextBox1.Text = vs[inc].Substring(0, tt);
+                tt++;
+            }
+
+        }
+
+        private void en_kk()
+        {
+            inc = random.Next(1, vs.Length);
+            guna2Button2.Text = vs[inc];
+            guna2TextBox1.Text = "";
+            tt = 0;
+
+        }
+
+        private void kk_en()
+        {
+            inc = random.Next(1, vj.Length);
+            guna2Button2.Text = vj[inc];
+            guna2TextBox1.Text = "";
+            tt = 0;
+
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (mode == 1)
+            {
+                if (vj[inc].ToLower().Trim() == guna2TextBox1.Text.ToLower().Trim())
+                {
+                    en_kk();
+                    
+                }
+            }
+            else
+            {
+                if (vs[inc].ToLower().Trim() == guna2TextBox1.Text.ToLower().Trim())
+                {
+                    kk_en();
+                }
+            }
+        }
+
+        private void tema1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mode = 1;
+            en_kk();
+            if (vs[inc].ToLower().Trim() == guna2TextBox1.Text.ToLower().Trim())
+            {
+                en_kk();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+
+
+
+
+
+            filename = openFileDialog1.FileName;
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filename);
+            Excel._Worksheet xlWorksheet = xlWorkbook.ActiveSheet as Excel.Worksheet;
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+            vs = new string[rowCount];
+            vj = new string[rowCount];
+            for (int i = 1; i < rowCount; i++)
+            {
+                vs[i] = Convert.ToString(xlRange.Cells[i, 1].Value);
+                vj[i] = Convert.ToString(xlRange.Cells[i, 2].Value);
+            }
+
+
+        }
+
+        private void tema1ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            mode = 2;
+            kk_en();
+            if (vj[inc].ToLower().Trim() == guna2TextBox1.Text.ToLower().Trim())
+            {
+                kk_en();
+            }
+        }
+
+
+    }
+}
